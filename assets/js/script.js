1e92bc984240a68bb5b6ae619348b6f7e33a282e
DanielMaherDev <dmdanielmaher@gmail.com>
2022-09-12 23:40:22 +0000

    add code to calculate two random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons){
+        button.addEventListener('click', function() {
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit!")
+            }
+            else{
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`)
+
+            }
+        } )
+    }
+})
+
 function rungame(){
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
 };
 function checkAnswer(){
@@ -15,11 +34,12 @@ function incrementWrongAnswer(){
 };
 function displayAdditionQuestion(){
 
+
 };
 function displaySubtractQuestion(){
 
 };
 function displayMultiplyQuestion(){
-    
+
 };
 
